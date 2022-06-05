d0e5eb12d74f14a17ba7ca2311b7f727f6db9f38
bằng hoàng <buibeo1234@gmail.com>
2022-06-05 16:01:07 +0700

    prettier hbs

--- src/routes/course.js
+++ src/routes/course.js
@@ -5,7 +5,6 @@ const courseController = require('../app/controller/courseController.js');
 
 router.get('/create', courseController.create);
 router.get('/edit', courseController.edit);
-
 router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
 
